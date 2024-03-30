cd7329f2cd6e9dddf230ef9dbde9c0f525da0f8c
Malinda <malinda.weerasinghe@betmakers.com>
2024-03-30 21:40:27 +0530

    Add basic home page

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+
+export default function Layout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <div className="min-h-full">
+        <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
+
+        <div className="flex flex-1 flex-col lg:pl-64">
+          <Topbar setSidebarOpen={setSidebarOpen} />
+
+          <main className="flex-1 pb-8">
+            <div className="mt-8 mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+            {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
